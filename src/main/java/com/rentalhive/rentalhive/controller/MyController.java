be7df016f48bf4304454fc6477e56db05c78c391
nouhailamadrani1 <nouhailamadrani2020@gmail.com>
2023-11-15 10:41:58 +0100

    modification of changelog

--- src/main/java/com/rentalhive/rentalhive/controller/MyController.java
+++ src/main/java/com/rentalhive/rentalhive/controller/MyController.java
@@ -1,2 +1,14 @@
-package com.rentalhive.rentalhive.controller;public class MyController {
+package com.rentalhive.rentalhive.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/rentalhive")
+    public String home() {
+
+        return "Hello from MyController!";
+    }
 }
--- src/main/java/com/rentalhive/rentalhive/controller/ReservationController.java
+++ src/main/java/com/rentalhive/rentalhive/controller/ReservationController.java
@@ -4,7 +4,6 @@ import com.rentalhive.rentalhive.model.Reservation;
 import com.rentalhive.rentalhive.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
